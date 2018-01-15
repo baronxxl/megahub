514fc99080ec77c9a6e8394fd0ed07d5884a6962
unknown <baronxxl1@gmail.com>
2018-01-15 20:13:54 +0300

    version 1.0

--- src/main/m.java
+++ src/main/m.java
@@ -3,75 +3,73 @@ package main;
 import java.util.Scanner;
 
 public class m {
+    public static final int CONTINUE = 0;
+    public static final int WIN_1 = 1;
+    public static final int WIN_2 = 2;
+    public static final int DRAW = 3;
+    public static final char PLAYER_1 ='+';
+    public static final char PLAYER_2='-';
+    public static final char EMPTY='@';
+
     public static void main(String[] args) {
         //oihhborthbjrtnb
 
-        char[][] gameField = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        char[][] gameField = {{EMPTY, EMPTY, EMPTY}, {EMPTY, EMPTY, EMPTY}, {EMPTY, EMPTY, EMPTY}};
         showField(gameField);
         int result;
-        char symbol = 'x';
+        char symbol = PLAYER_1;
         do {
             move(gameField, symbol);
-            symbol = symbol == 'x' ? '0' : 'x';
+            symbol = symbol == PLAYER_1 ? PLAYER_2 : PLAYER_1;
             showField(gameField);
             result = checkField(gameField);
 
 
-        } while (result == 0);
-        switch (result) {
-            case 1:
-                System.out.println("pobeda X");
-                break;
-            case 2:
-                System.out.println("pobeda 0");
-                break;
-            case 3:
-                System.out.println("ni4'9");
-                break;
-        }
+        } while (result == CONTINUE);
 
+        showResult(result);
 
     }
 
-    public static int checkField(char[][] gameField) {
+    public static int checkField(char[][] gameField) {      //Проверка условий победы
 
         for (int i = 0; i < 3; i++) {
             if (gameField[i][0] == gameField[i][1] && gameField[i][1] == gameField[i][2]) {
-                if (gameField[i][0] == 'x') {
-                    return 1;
+                if (gameField[i][0] == PLAYER_1) {
+                    return WIN_1;
                 }
-                if (gameField[i][0] == '0') {
-                    return 2;
+                if (gameField[i][0] == PLAYER_2) {
+                    return WIN_2;
                 }
             }
             if (gameField[0][i] == gameField[1][i] && gameField[1][i] == gameField[2][i]) {
-                if (gameField[0][i] == 'x') {
-                    return 1;
+                if (gameField[0][i] == PLAYER_1) {
+                    return WIN_1;
                 }
-                if (gameField[0][i] == '0') {
-                    return 2;
+                if (gameField[0][i] == PLAYER_2) {
+                    return WIN_2;
 
 
                 }
             }
         }
         if (gameField[0][0] == gameField[1][1] && gameField[1][1] == gameField[2][2]) {
-            if (gameField[0][0] == 'x') return 1;
-            if (gameField[0][0] == '0') return 2;
+            if (gameField[0][0] == PLAYER_1) return WIN_1;
+            if (gameField[0][0] == PLAYER_2) return WIN_2;
         }
         if (gameField[0][2] == gameField[1][1] && gameField[1][1] == gameField[2][0]) {
-            if (gameField[0][0] == 'x') return 1;
-            if (gameField[0][0] == '0') return 2;
+            if (gameField[0][0] == PLAYER_1) return WIN_1;
+            if (gameField[0][0] == PLAYER_2) return WIN_2;
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (gameField[i][j] == ' ') return 0;
+                if (gameField[i][j] == EMPTY) return CONTINUE;
             }
         }
-        return 3;
+        return DRAW;
     }
 
-    public static void move(char[][] gameField, char symbol) {
+    public static void move(char[][] gameField, char symbol) {  // Ход
         Scanner in = new Scanner(System.in);
         int n, m, number;
         do {
@@ -81,13 +79,13 @@ public class m {
             n = 2 - (number - 1) / 3;
             m = (number - 1) % 3;
 
-        } while (number < 1 || number > 9 || gameField[n][m] != ' ');
+        } while (number < 1 || number > 9 || gameField[n][m] != EMPTY);
         gameField[n][m] = symbol;
         System.out.println();
 
     }
 
-    public static void showField(char a[][]) {
+    public static void showField(char a[][]) { //Игровое поле
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(" " + a[i][j] + " ");
@@ -103,6 +101,21 @@ public class m {
         }
         System.out.println();
 
+
+    }
+
+    public static void showResult(int result) { // Вывод итогов игры
+        switch (result) {
+            case WIN_1:
+                System.out.println("Победа "+PLAYER_1);
+                break;
+            case WIN_2:
+                System.out.println("Победа "+PLAYER_2);
+                break;
+            case DRAW:
+                System.out.println("Ничья");
+                break;
+        }
 
     }
 }
